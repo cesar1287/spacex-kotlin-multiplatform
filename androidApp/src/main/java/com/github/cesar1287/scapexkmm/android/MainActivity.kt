231cd7a4a6104c9e754a3293378db89e1ded62c1
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-05 20:12:40 -0300

    added ios rocketlaunch row

--- androidApp/src/main/java/com/github/cesar1287/scapexkmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/github/cesar1287/scapexkmm/android/MainActivity.kt
@@ -8,12 +8,11 @@ import com.github.cesar1287.scapexkmm.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //todo Finalizar design
-    //todo loading
-    //todo implementar com flow
     //todo Fazer iOS
-    //todo koin no KMM
+    //todo loading
     //todo Hilt no Android
+    //todo koin no KMM
+    //todo implementar com flow
 
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
