62088b1b2c11ee7471a7cbba4ddd2f548e5cb934
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-02-05 20:24:24 -0300

    finishing ios app and updating android app name

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,62 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    
+    @ObservedObject private(set) var viewModel: ViewModel
 
-	var body: some View {
-		Text(greet)
-	}
+    var body: some View {
+      NavigationView {
+          listView()
+          .navigationBarTitle("SpaceX Launches")
+          .navigationBarItems(trailing:
+              Button("Reload") {
+                  self.viewModel.loadLaunches()
+          })
+      }
+    }
+
+    private func listView() -> AnyView {
+      switch viewModel.launches {
+      case .loading:
+          return AnyView(Text("Loading...").multilineTextAlignment(.center))
+      case .result(let launches):
+          return AnyView(List(launches) { launch in
+              RocketLaunchRow(rocketLaunch: launch)
+          })
+      case .error(let description):
+          return AnyView(Text(description).multilineTextAlignment(.center))
+      }
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+extension ContentView {
+
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+
+    class ViewModel: ObservableObject {
+        let sdk: LaunchesUseCase
+        @Published var launches = LoadableLaunches.loading
+
+        init(sdk: LaunchesUseCase) {
+            self.sdk = sdk
+            self.loadLaunches()
+        }
+
+        func loadLaunches() {
+            self.launches = .loading
+            sdk.getAllLaunches( completionHandler: { launches, error in
+                if let launches = launches {
+                    self.launches = .result(launches)
+                } else {
+                    self.launches = .error(error?.localizedDescription ?? "error")
+                }
+            })
+        }
+    }
 }
+
+extension RocketLaunch: Identifiable { }
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,12 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    let sdk = LaunchesUseCaseImpl()
+    var body: some Scene {
+        WindowGroup {
+            ContentView(viewModel: .init(sdk: sdk))
+        }
+    }
 }
